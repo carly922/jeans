a9d723154e712cb94d9224760f6315a74286e0af
Carly <77548696+carly922@users.noreply.github.com>
2026-07-11 12:26:49 -0400

    added mySQL functionality and optimized

--- script1.py
+++ script1.py
@@ -9,15 +9,8 @@ mUser = "root"
 mPassword = "root"
 mDatabase = "jeans"
 
-rootUrl= ''
-itemUrl = ''
-itemImg = ''
-itemPID = ''
-itemName = ''
-itemPrice = 0.00
-
-def scrape():
-    url = "https://oldnavy.gap.com/browse/product.do?pid=846874002&vid=1&pcid=85729&cid=85729&nav=meganav%3AWomen%3AWomen%27s+Bottoms%3AJeans#pdp-page-content"
+def scrape(url):
+    #url = "https://oldnavy.gap.com/browse/product.do?pid=846874002&vid=1&pcid=85729&cid=85729&nav=meganav%3AWomen%3AWomen%27s+Bottoms%3AJeans#pdp-page-content"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -25,6 +18,10 @@ def scrape():
     itemName = (soup.title.string).split(" | ")[0]
     #finds item URL
     itemUrl = soup.find("link", rel="canonical")["href"]
+
+    #extracts PID from canonical link (itemURL)
+    itemPID = itemUrl.split("pid=",1)[1]
+
     #strips the item URL to get it down to the base webpage URL
     rootUrl = itemUrl.rsplit('/', 2)[0]
 
@@ -33,27 +30,39 @@ def scrape():
 
     #combines base URL and img src to create a full link
     itemImg = rootUrl + imageStem
-    print(itemImg)
 
     #finds string of current sale price on page
     priceString = soup.find(class_=re.compile("current-sale-price")).text
     #converts price to a numerical value
     itemPrice = float(priceString.replace("$", ""))
 
+    return itemName, itemUrl, itemPID, rootUrl, itemImg, itemPrice
+
 def connect():
-    return mysql.connector.connect(
-        host=mHost,
-        user=mUser,
-        password=mPassword,
-        database=mDatabase
-    )
+    return mysql.connector.connect( host=mHost, user=mUser, password=mPassword, database=mDatabase )
 
+def addItem(itemName, itemUrl, itemPID, rootUrl, itemImg, itemPrice):
+    #VALUES (
+    #   p_rootUrl,
+    #   p_itemUrl,
+    #   p_itemImg,
+    #   p_itemPid,
+    #   p_itemName,
+    #   p_itemPrice
+    #);
+    conn = connect()
+    cursor = conn.cursor()
 
-#def pushtoDatabase():
+    cursor.callproc("AddItem", [rootUrl, itemUrl, itemImg, itemPID, itemName, itemPrice])
+    conn.commit()
+
+    print("Item " + itemName + " added.")
+    conn.close()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    scrape()
-
+    url = input("Paste the full URL of your item: ")
+    item = scrape(url)
+    addItem(*item)
 
