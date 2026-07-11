2de9cc226aced370f3975b0bad18452bebb58f46
Carly <77548696+carly922@users.noreply.github.com>
2026-07-11 11:28:39 -0400

    added image scraping capability

--- script1.py
+++ script1.py
@@ -11,6 +11,7 @@ mDatabase = "jeans"
 
 rootUrl= ''
 itemUrl = ''
+itemImg = ''
 itemPID = ''
 itemName = ''
 itemPrice = 0.00
@@ -19,14 +20,20 @@ def scrape():
     url = "https://oldnavy.gap.com/browse/product.do?pid=846874002&vid=1&pcid=85729&cid=85729&nav=meganav%3AWomen%3AWomen%27s+Bottoms%3AJeans#pdp-page-content"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    #stores page title, usually includes item name
-    itemName = (soup.title.string)
 
+    #stores page title, usually includes item name IN THIS CASE it splits off the brand name
+    itemName = (soup.title.string).split(" | ")[0]
     #finds item URL
     itemUrl = soup.find("link", rel="canonical")["href"]
-
     #strips the item URL to get it down to the base webpage URL
     rootUrl = itemUrl.rsplit('/', 2)[0]
+
+    #find img source
+    imageStem = soup.find("img", alt=lambda x: x and itemName.lower() in x.lower())["src"]
+
+    #combines base URL and img src to create a full link
+    itemImg = rootUrl + imageStem
+    print(itemImg)
 
     #finds string of current sale price on page
     priceString = soup.find(class_=re.compile("current-sale-price")).text
