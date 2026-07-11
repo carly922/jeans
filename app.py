719f347e9f6af2cb570bf659df211d8fc6df8a1d
Carly <77548696+carly922@users.noreply.github.com>
2026-07-11 13:15:33 -0400

    added website and backend

---
+++ app.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_items():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="jeans"
+    )
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM jeanData")
+    items = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    return items
+
+@app.route("/")
+def home():
+    items = get_items()
+    print(items)   # add this
+    return render_template("index.html", items=items)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
